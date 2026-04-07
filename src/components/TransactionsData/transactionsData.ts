fd5b2ba758b036a284510332c3a0b3e797eea21b
nazar1panyushchyk <npanusik@gmail.com>
2026-04-07 19:12:22 +0300

    fixed details, final2 commit

--- src/components/TransactionsData/transactionsData.ts
+++ src/components/TransactionsData/transactionsData.ts
@@ -3,25 +3,9 @@ import type { TransactionsProps } from "../TransactionsTypes/transactionsTypes";
 export const expenseData: TransactionsProps = {
   description: "Опис товару",
   category: "Категорія товару",
-  summary: [
-    { id: 1, month: "ЛИСТОПАД", amount: "10 000.00" },
-    { id: 2, month: "ЖОВТЕНЬ", amount: "30 000.00" },
-    { id: 3, month: "ВЕРЕСЕНЬ", amount: "10 000.00" },
-    { id: 4, month: "СЕРПЕНЬ", amount: "10 000.00" },
-    { id: 5, month: "ЛИПЕНЬ", amount: "10 000.00" },
-    { id: 6, month: "ЧЕРВЕНЬ", amount: "10 000.00" },
-  ],
 };
 
 export const incomeData: TransactionsProps = {
   description: "Опис прибутку",
   category: "Категорія прибутку",
-  summary: [
-    { id: 1, month: "ЛИСТОПАД", amount: "25 500.00" },
-    { id: 2, month: "ЖОВТЕНЬ", amount: "25 500.00" },
-    { id: 3, month: "ВЕРЕСЕНЬ", amount: "25 500.00" },
-    { id: 4, month: "СЕРПЕНЬ", amount: "20 000.00" },
-    { id: 5, month: "ЛИПЕНЬ", amount: "20 000.00" },
-    { id: 6, month: "ЧЕРВЕНЬ", amount: "18 000.00" },
-  ],
 };
--- src/components/TransactionsTypes/transactionsTypes.ts
+++ src/components/TransactionsTypes/transactionsTypes.ts
@@ -1,14 +1,4 @@
 export type TransactionsProps = {
   description: string;
   category: string;
-  // categories: {
-  //   value: string;
-  //   label: string;
-  //   amount: string;
-  // }[];
-  summary: {
-    id: number;
-    month: string;
-    amount: string;
-  }[];
 };
